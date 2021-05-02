bb957a9f345243fae4e07205973d92e0c26f3c13
Gianmarco Caputo <gianmarco.caputo.uk@gmail.com>
2021-05-02 23:53:42 +0100

    Remove unnecessary cout calls.

--- src/DirectoryReader.cpp
+++ src/DirectoryReader.cpp
@@ -12,10 +12,8 @@ vector<File> DirectoryReader::get_all_files (const string &dir_path)
   for (const auto &entry : directory_iterator (dir_path))
   {
 	File file (entry.path ().string ());
-	cout << "File found: " + file.get_name () << endl;
 	files.push_back (file);
   }
-  cout << "Total files found: " << files.size () << endl;
-  cout << "Starting tailer.." << endl << endl;
+  cout << "Files found: " << files.size () << endl;
   return files;
 }
