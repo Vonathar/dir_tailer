bddb5fb729dc0c0cdffea1a58e38093bc0b11aad
Gianmarco Caputo <gianmarco.caputo.uk@gmail.com>
2021-05-02 18:53:38 +0100

    Implement DirectoryReader class.

---
+++ src/DirectoryReader.cpp
@@ -0,0 +1,21 @@
+#include "DirectoryReader.h"
+#include <filesystem>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::filesystem::directory_iterator;
+
+vector<File> DirectoryReader::read_all_files (const string &dir_path)
+{
+  vector<File> files;
+  for (const auto &entry : directory_iterator (dir_path))
+  {
+	File file (entry.path ().string ());
+	cout << "File found: " + file.get_name () << endl;
+	files.push_back (file);
+  }
+  cout << "Total files found: " << files.size () << endl;
+  cout << "Starting tailer.." << endl << endl;
+  return files;
+}
---
+++ src/DirectoryReader.h
@@ -0,0 +1,26 @@
+#ifndef _DIRECTORYREADER_H_
+#define _DIRECTORYREADER_H_
+
+#include "File.h"
+#include <iostream>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+/**
+ * A class that represents a way to read files from a directory.
+ */
+class DirectoryReader {
+
+ public:
+  /**
+   * Returns the files found in the directory at the given path.
+   *
+   * @param dir_path the path to the directory.
+   * @return the files found in the directory.
+   */
+  vector<File> read_all_files (const string &dir_path);
+};
+
+#endif//_DIRECTORYREADER_H_
